95b84258668159e95691bd373483b0a3297d2225
Diogo-Freitas <diogofreitasdf@hotmail.com>
2024-12-04 15:10:05 -0300

    Implementação de interface com SingleChildScrollView

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -24,7 +24,48 @@ class _MyHomePageState extends State<MyHomePage> {
               color: Colors.white),
         ],
       ),
-      body: const Center(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Icon(Icons.person_outline, size: 120, color: Colors.green),
+            const SizedBox(height: 16),
+            const TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Peso (kg)",
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25),
+            ),
+            const SizedBox(height: 16),
+            const TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Altura (cm)",
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  padding: const EdgeInsets.symmetric(vertical: 10)),
+              child: const Text('Calcular',
+                  style: TextStyle(color: Colors.white, fontSize: 25)),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'Informe seus dados!',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
